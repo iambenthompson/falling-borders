46d2b517ff7f39c84c69c1812e6a80dadd39fd39
Ben Thompson <git@iambenthompson.com>
2017-12-18 10:57:28 -0500

    Converts/upgrades to webpack project, serve with "yarn run start" and test on mobile

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: "./app.js", // bundle's entry point
+    entry: {
+        app: './src/app.js' // bundle's entry point
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'), // output directory
-        filename: "[name].js" // name of the generated bundle
-    }
+        path: path.join(__dirname, 'dist'), // output directory
+        filename: '[name].bundle.js' // name of the generated bundle
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html',
+        inject: 'body'
+    })],
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/,
+            options: {
+                presets: ['env']
+            }
+        }]
+    },
+    devServer: {
+        host: '0.0.0.0',
+        port: 8080,
+        contentBase: path.resolve(__dirname, './dist'),
+        stats: 'errors-only',
+        disableHostCheck: true
+    }    
 };
